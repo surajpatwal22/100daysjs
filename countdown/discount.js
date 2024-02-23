943f54b23ca78fa9038315bb63ef6731b6fd0655
surajpatwal22 <patwal0suraj@gmail.com>
2024-02-24 00:00:13 +0530

    count

---
+++ countdown/discount.js
@@ -0,0 +1,10 @@
+var emial = true;
+var google = false;
+var facebook = false ;
+
+if (emial||google||facebook) {
+    console.log('login sucessfully');
+    
+} else {
+    console.log('error');
+}
